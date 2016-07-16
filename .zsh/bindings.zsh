992800cc476448590f3d68889488c4b19b707c21
Eric Freese <ericdfreese@gmail.com>
2016-07-16 15:28:12 -0600

    New keybindings

--- .zsh/bindings.zsh
+++ .zsh/bindings.zsh
@@ -1,2 +1,16 @@
+bindkey '\e[3~' delete-char
+
 bindkey '\e[A' history-beginning-search-backward
 bindkey '\e[B' history-beginning-search-forward
+
+function autosuggest_accept_or_expand() {
+	if [ -n "$POSTDISPLAY" ]; then
+		zle -w autosuggest-accept
+	else
+		zle -w expand-or-complete
+	fi
+}
+
+zle -N autosuggest_accept_or_expand
+
+bindkey '^ ' autosuggest_accept_or_expand
