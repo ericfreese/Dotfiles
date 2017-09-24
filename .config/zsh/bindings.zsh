3e7839418796f2e6eaca0f02facb97c553d6fcf8
Eric Freese <ericdfreese@gmail.com>
2017-09-24 17:58:07 -0600

    C-p and C-n should also use special history search widget

--- .config/zsh/bindings.zsh
+++ .config/zsh/bindings.zsh
@@ -1,4 +1,6 @@
 bindkey '\e[3~' delete-char
 
 bindkey '\e[A' history-beginning-search-backward
+bindkey '^P' history-beginning-search-backward
 bindkey '\e[B' history-beginning-search-forward
+bindkey '^N' history-beginning-search-forward
